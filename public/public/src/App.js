0470928586776dc6440efcc6aba71905d0262cc2
Ganza Aimee Daniella <daniellaganza30@gmail.com>
2023-09-17 11:09:28 +0200

    initial commit

---
+++ public/public/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import  Inputtodo from './components/Inputtodo';
+import ResTodo from './components/ResTodo';
+
+function App() {
+  return (
+    <div className='container'>
+     
+      <Inputtodo/>
+      <ResTodo/>
+    </div>
+  );
+}
+
+export default App;
